950f1aa0ff92e375c36b4e7f7cdbe5864a227436
alexhogue <alexandra_hogue@brown.edu>
2025-10-07 13:48:13 -0400

    fixed cursor effect boundaries

--- exercise6/door.js
+++ exercise6/door.js
@@ -62,45 +62,39 @@ document.addEventListener("DOMContentLoaded", function () {
       cursor.style.top = y + "px";
     });
 
+
+    function getRandomTransitionVal() {
+      return `${Math.random() * 200 - 100}px`;
+    }
+
+    function getRandomScale() {
+      return `${Math.random() * (2 - 0.5) + 0.5}`;
+    }
+
+    function getRandomColor() {
+      const red = Math.floor(Math.random() * 250); // Random integer between 0 and 255
+      const green = Math.floor(Math.random() * 120);
+      const blue = Math.floor(Math.random() * 200);
+      const color = `rgb(${red}, ${green}, ${blue})`;
+      return color;
+    }
+
+    special_doorframe.addEventListener("mousemove", (e) => {
+      let i = document.createElement("i");
+      const rect = special_door.getBoundingClientRect();
+      i.style.left = `${e.clientX - rect.left}px`;
+      i.style.top = `${e.clientY - rect.top}px`;
+      i.style.setProperty("--scale", getRandomScale());
+      i.style.setProperty("--random-color", randomColor({ hue: "pink" }));
+      i.style.setProperty(`--x`, getRandomTransitionVal());
+      i.style.setProperty(`--y`, getRandomTransitionVal());
+
+      special_doorframe.appendChild(i);
+
+      setTimeout(() => {
+        special_doorframe.removeChild(i);
+      }, 800);
+    });
+
+
 });
-
-const special_doorframe = document.getElementById("special-doorframe");
-function confetti(event) {
-  let i = document.createElement("i");
-  const rect = special_doorframe.getBoundingClientRect();
-  i.style.left = `${event.clientX - rect.left}px`;
-  i.style.top = `${event.clientY - rect.top}px`;
-  const scale = `${Math.random() * 3}px`;
-  i.style.transform = `scale(${scale})`;
-  i.style.setProperty("--scale", getRandomScale());
-  i.style.setProperty(
-    "--random-color",
-    randomColor({ hue: "pink"})
-  );
-  i.style.setProperty(`--x`, getRandomTransitionVal());
-  i.style.setProperty(`--y`, getRandomTransitionVal());
-
-  special_doorframe.appendChild(i);
-
-  setTimeout(() => {
-    special_doorframe.removeChild(i);
-  }, 800);
-}
-
-function getRandomTransitionVal() {
-  return `${Math.random() * 200 - 100}px`;
-}
-
-function getRandomScale() {
-  return `${Math.random() * (2 - 0.5) + 0.5}`;
-}
-
-function getRandomColor() {
-  const red = Math.floor(Math.random() * 250); // Random integer between 0 and 255
-  const green = Math.floor(Math.random() * 120);
-  const blue = Math.floor(Math.random() * 200);
-  const color = `rgb(${red}, ${green}, ${blue})`;
-  return color;
-}
-
-special_doorframe.addEventListener("mousemove", confetti);
